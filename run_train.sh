0ba2cfd44b1a9361dd4a503dd8fb0ea70d4ee027
raraduck <raraducker@gmail.com>
2024-10-08 11:09:25 +0900

    update run_train for test

--- run_train.sh
+++ run_train.sh
@@ -1,20 +1,18 @@
 python scripts/run_train.py \
-    --rounds 5 \
-    --round 0 \
-    --epochs 10 \
-    \
-    --zoom \
-    --resize 128 \
-    --patch_size 128 \
-    --dataset CC359PPMI \
-    --data_root cc359ppmi \
-    --inst_root inst_0* \
-    \
-    --cases_split cc359ppmi/CC359PPMI.csv \
-    --inst_ids [-1] \
-    --batch_size 1 \
-    --use_gpu \
-    \
+	--rounds 0 \
+	--round 0 \
+	--epochs 2 \
+	\
+	--resize 128 \
+	--patch_size 128 \
+	--dataset CC359PPMI \
+	--data_root cc359ppmi128 \
+	--inst_root inst_0* \
+	\
+	--cases_split cc359ppmi/CC359PPMI_v1.csv \
+	--inst_ids [1] \
+	--batch_size 1 \
+	\
 	--input_channel_names [t1] \
 	--label_groups [[26,26],[58,58],[11,11],[50,50],[12,12],[51,51]] \
 	--label_names [LA,RA,LC,RC,LP,RP] \
@@ -22,7 +20,7 @@ python scripts/run_train.py \
 	--unet_arch unet \
 	--channels_list [32,64,128,256] \
 	--block res \
-    --optim adamw \
+	--optim adamw \
 	--ds_layer 1 \
 	--kernel_size 3 \
 	--norm instance
