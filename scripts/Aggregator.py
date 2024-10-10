4f8f55f12e05063bffd9e013fd6f6763bbc2b756
dwnusa-win10 <dwnusa@gmail.com>
2024-10-11 02:04:58 +0900

    v0.4.6.1 updated fedavg test code

--- scripts/Aggregator.py
+++ scripts/Aggregator.py
@@ -1,8 +1,23 @@
 import torch
 from copy import deepcopy
 
-def fedavg():
-    print('hi fedavg')
+def fedavg(path_list):
+    local_models_with_dlen = [torch.load(m) for m in path_list]
+    # local_dlen = [el['P'] for el in local_models_with_dlen]
+    local_dlen = [1 for el in local_models_with_dlen]
+    local_dprob = [el/sum(local_dlen) for el in local_dlen]
+    local_models = [el['model'] for el in local_models_with_dlen]
+    first_model = local_models[0]  # 첫 번째 모델을 기준 구조로 사용
+
+    # 파라미터 평균화를 위한 빈 껍데기 딕셔너리 생성
+    global_state_dict = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.items()}
+
+    # 모든 로컬 모델 파라미터의 합 계산
+    for dprob, model_state_dict in zip(local_dprob, local_models):
+        local_state_dict = model_state_dict
+        for key in global_state_dict.keys():
+            global_state_dict[key] += local_state_dict[key] * dprob
+    return global_state_dict
 
 # def fedavg(local_models_with_dlen):
 #     """ 모든 로컬 모델의 파라미터를 평균화합니다. """
--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -12,24 +12,30 @@ parser.add_argument('--round', type=int, default=0)
 parser.add_argument('--method', type=str, default="fedavg", 
     choices=['fedavg', 'fedpid', 'fedpod'], help='type of avg')
 parser.add_argument('--job_id', type=str, default="")
-parser.add_argument('--inst_ids', type=parse_1d_int_list, default=[])
+parser.add_argument('--inst_id', type=int, default=0)
 
 if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
     if args.method == "fedavg":
-        # base_path = os.path.join("states", f"R{args.rounds:02}r{args.round:02}")
-        # print(base_path)
-        # pth_list = [os.path.join(base_path, job, 'models', '*_last.pth') for idx, job in enumerate(args.job_ids,1) if idx in args.inst_ids]
-        # # pth_list = [os.path.join(base_path, inst) for inst in args.job_ids]
-        # # pth_list = [os.path.join(base_path, job) for idx, job in enumerate(args.job_ids,1) if str(idx) in args.inst_ids]
-        # # # fedavg()
-        # print(pth_list)
         base_dir = os.path.join('/','fedpod','states')
-        pattern = os.path.join(base_dir, f"R{args.rounds:02}r{args.round:02}", f"{args.job_id}*", 'models', '*_last.pth')
-        trg_pth = glob.glob(pattern)
+        # base_dir = os.path.join('.','states')
+        round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{args.round:02}")
+        inst_dir = os.path.join(round_dir, f"{args.job_id}*")
+        pattern = os.path.join(inst_dir, 'models', '*_last.pth')
+        pth_path = glob.glob(pattern)
         print(pattern)
-        print(trg_pth)
-        fedavg()
+        print(pth_path)
+        global_state_dict = fedavg(pth_path)
+        state = {'model': global_state_dict}
+
+        # exp_folder = f"{self.cli_args.exp_name}"
+        # save_model_path1 = os.path.join("states", exp_folder)
+        # save_model_path2 = f"round_{self.round:02d}"
+        # save_model_path3 = f"epoch_{from_epoch:03d}"
+        next_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{args.round+1:02}")
+        os.makedirs(next_round_dir, exist_ok=True)
+        save_model_path = os.path.join(next_round_dir, f"R{args.rounds:02}r{args.round+1:02}.pth")
+        torch.save(state, save_model_path)
     else:
         raise NotImplementedError(f"{args.method.upper()} is not implemented on Aggregator()")
     
