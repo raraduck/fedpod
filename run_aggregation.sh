a43ca0392b159eec4da38d3cea9d59624a2338b3
dwnusa-win10 <dwnusa@gmail.com>
2024-10-11 03:17:44 +0900

    v0.4.7 created for run_aggregation placed at the head of round

--- run_aggregation.sh
+++ run_aggregation.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 rounds=""
 round=""
-method=""
+algorithm=""
 job_name=""
 inst_id=""
+model_pth=""
 
 # 명령줄 옵션 처리
-while getopts R:r:m:j:i: option
+while getopts R:r:a:j:i:m: option
 do
     case "${option}"
     in
         R) rounds=${OPTARG};;
         r) round=${OPTARG};;
-		m) method=${OPTARG};;
+		a) algorithm=${OPTARG};;
         j) job_name=${OPTARG};;
         i) inst_id=${OPTARG};;
+		m) model_pth=${OPTARG};;
     esac
 done
 
 # 필수 옵션 검사
 if [ -z "$rounds" ] || [ -z "$round" ] || [ -z "$method" ] || [ -z "$job_name" ] || [ -z "$inst_id" ]; then
     echo "Error: All parameters are required."
-    echo "Usage: $0 -R <rounds> -r <round> -m <method> -j <job_name> -i <inst_id>"
+    echo "Usage: $0 -R <rounds> -r <round> -a <algorithm> -j <job_name> -i <inst_id> -m <model_pth>"
     exit 1
 fi
 
-echo "rounds: $rounds, round: $round, job: $job_name, inst: $inst_id, method: $method"
+echo "rounds: $rounds, round: $round, algorithm: $algorithm, job: $job_name, inst: $inst_id, model_pth: $model_pth"
 
 python3 scripts/run_aggregation.py \
 	--rounds $rounds \
 	--round $round \
-	--method $method \
+	--algorithm $algorithm \
 	--job_id $job_name \
-	--inst_id $inst_id
+	--inst_id $inst_id \
+	--weight_path $model_pth
--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import time
 import argparse
 import sys
@@ -13,29 +14,39 @@ parser.add_argument('--method', type=str, default="fedavg",
     choices=['fedavg', 'fedpid', 'fedpod'], help='type of avg')
 parser.add_argument('--job_id', type=str, default="")
 parser.add_argument('--inst_id', type=int, default=0)
+parser.add_argument('--weight_path', type=str, default="None",
+    help='path to pretrained encoder or decoder weight, None for train-from-scratch')
 
 if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
+    prev_round = args.round - 1
+    curr_round = args.round
+    base_dir = os.path.join('/','fedpod','states')
     if args.method == "fedavg":
-        base_dir = os.path.join('/','fedpod','states')
-        # base_dir = os.path.join('.','states')
-        round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{args.round:02}")
-        inst_dir = os.path.join(round_dir, f"{args.job_id}*")
-        pattern = os.path.join(inst_dir, 'models', '*_last.pth')
-        pth_path = glob.glob(pattern)
-        # print(pattern)
-        # print(pth_path)
-        global_state_dict = fedavg(pth_path)
-        state = {'model': global_state_dict}
-
-        # exp_folder = f"{self.cli_args.exp_name}"
-        # save_model_path1 = os.path.join("states", exp_folder)
-        # save_model_path2 = f"round_{self.round:02d}"
-        # save_model_path3 = f"epoch_{from_epoch:03d}"
-        next_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{args.round+1:02}")
-        os.makedirs(next_round_dir, exist_ok=True)
-        save_model_path = os.path.join(next_round_dir, f"R{args.rounds:02}r{args.round+1:02}.pth")
-        torch.save(state, save_model_path)
+        if prev_round < 0:
+            orig_file = args.cli_args.weight_path
+            curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
+            os.makedirs(curr_round_dir, exist_ok=True)
+            save_model_path = os.path.join(curr_round_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
+            shutil.copy2(orig_file, save_model_path)
+        else:
+            # base_dir = os.path.join('.','states')
+            prev_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{prev_round:02}")
+            inst_dir = os.path.join(prev_round_dir, f"{args.job_id}*")
+            pattern = os.path.join(inst_dir, 'models', '*_last.pth')
+            pth_path = glob.glob(pattern)
+            # print(pattern)
+            # print(pth_path)
+            global_state_dict = fedavg(pth_path)
+            state = {'model': global_state_dict}
+            # exp_folder = f"{self.cli_args.exp_name}"
+            # save_model_path1 = os.path.join("states", exp_folder)
+            # save_model_path2 = f"round_{self.round:02d}"
+            # save_model_path3 = f"epoch_{from_epoch:03d}"
+            curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
+            os.makedirs(curr_round_dir, exist_ok=True)
+            save_model_path = os.path.join(curr_round_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
+            torch.save(state, save_model_path)
     else:
         raise NotImplementedError(f"{args.method.upper()} is not implemented on Aggregator()")
     
