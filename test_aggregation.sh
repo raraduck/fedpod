30ffde2dd41815aa257999fdc47a412d21e6d4f9
raraduck <raraducker@gmail.com>
2024-10-11 13:43:31 +0900

    update

--- test_aggregation.sh
+++ test_aggregation.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-echo "initial aggregation requires initial_model"
-echo "initial_model is just copied to under states/job-step folder"
-source run_aggregation.sh \
-    -R 2 \
-    -r 0 \
-    -a fedavg \
-    -j test \
-    -i 0 \
-    -m /fedpod/cc359ppmi128/R00E000.pth \
-&& \
-rm -rf /fedpod/states/R02r00/test00
-# echo "second aggregation requires to not specify model_path"
-# echo "which is to collect them(local models) by round and job_prefix pattern"
-# &&
+# echo "initial aggregation requires initial_model"
+# echo "initial_model is just copied to under states/job-step folder"
 # source run_aggregation.sh \
 #     -R 2 \
 #     -r 0 \
 #     -a fedavg \
 #     -j test \
-#     -i 0
+#     -i 0 \
+#     -m /fedpod/cc359ppmi128/R00E000.pth \
+# && \
+# rm -rf /fedpod/states/R02r00/test00 \
+
+echo "second aggregation requires to not specify model_path" \
+
+echo "which is to collect them(local models) by round and job_prefix pattern" \
+
+source run_aggregation.sh \
+    -R 2 \
+    -r 0 \
+    -a fedavg \
+    -j test \
+    -i 0
