36834c8caa79824ea5649131a59a06487703960a
dwnusa-win10 <dwnusa@gmail.com>
2024-10-11 22:43:16 +0900

    v0.4.12.2: fedavg, fedwavg ready and metric saved to csv (DSCL, DICE, HD95)

--- scripts/Aggregator.py
+++ scripts/Aggregator.py
@@ -1,38 +1,44 @@
 import torch
 from copy import deepcopy
 
-def fedavg(path_list):
-    local_models_with_dlen = [torch.load(m) for m in path_list]
-    # local_dlen = [el['P'] for el in local_models_with_dlen]
-    local_dlen = [1 for el in local_models_with_dlen]
-    local_dprob = [el/sum(local_dlen) for el in local_dlen]
-    local_models = [el['model'] for el in local_models_with_dlen]
-    first_model = local_models[0]  # 첫 번째 모델을 기준 구조로 사용
+def fedavg(weights, models):
+    first_model = models[0]  # 첫 번째 모델을 기준 구조로 사용
 
     # 파라미터 평균화를 위한 빈 껍데기 딕셔너리 생성
-    global_state_dict = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.items()}
+    agg_m = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.items()}
 
     # 모든 로컬 모델 파라미터의 합 계산
-    for dprob, model_state_dict in zip(local_dprob, local_models):
-        local_state_dict = model_state_dict
-        for key in global_state_dict.keys():
-            global_state_dict[key] += local_state_dict[key] * dprob
-    return global_state_dict
+    for w, m in zip(weights, models):
+        for key in agg_m.keys():
+            agg_m[key] += m[key] * w
+    return agg_m
 
-# def fedavg(local_models_with_dlen):
-#     """ 모든 로컬 모델의 파라미터를 평균화합니다. """
-#     local_dlen = [1.0 for el in local_models_with_dlen]
+def fedwavg(weights, models):
+    first_model = models[0]  # 첫 번째 모델을 기준 구조로 사용
+
+    # 파라미터 평균화를 위한 빈 껍데기 딕셔너리 생성
+    agg_m = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.items()}
+
+    # 모든 로컬 모델 파라미터의 합 계산
+    for w, m in zip(weights, models):
+        for key in agg_m.keys():
+            agg_m[key] += m[key] * w
+    return agg_m
+
+
+# def fedwavg(path_list):
+#     local_models_with_dlen = [torch.load(m) for m in path_list]
+#     local_dlen = [el['P'] for el in local_models_with_dlen]
 #     local_dprob = [el/sum(local_dlen) for el in local_dlen]
-#     local_models = [el[1] for el in local_models_with_dlen]
+#     local_models = [el['model'] for el in local_models_with_dlen]
 #     first_model = local_models[0]  # 첫 번째 모델을 기준 구조로 사용
 
 #     # 파라미터 평균화를 위한 빈 껍데기 딕셔너리 생성
-#     global_state_dict = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.state_dict().items()}
+#     global_state_dict = {key: torch.zeros_like(value, dtype=torch.float) for key, value in first_model.items()}
 
 #     # 모든 로컬 모델 파라미터의 합 계산
-#     for dprob, model in zip(local_dprob, local_models):
-#         local_state_dict = model.state_dict()
+#     for dprob, model_state_dict in zip(local_dprob, local_models):
+#         local_state_dict = model_state_dict
 #         for key in global_state_dict.keys():
 #             global_state_dict[key] += local_state_dict[key] * dprob
 #     return global_state_dict
-
--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--rounds', type=int, default=5)
 parser.add_argument('--round', type=int, default=0)
 parser.add_argument('--algorithm', type=str, default="fedavg", 
-    choices=['fedavg', 'fedpid', 'fedpod'], help='type of avg')
+    choices=['fedavg', 'fedwavg', 'fedpid', 'fedpod'], help='type of avg')
 parser.add_argument('--job_prefix', type=str, default="")
 parser.add_argument('--inst_id', type=int, default=0)
 parser.add_argument('--weight_path', type=str, default="None",
@@ -26,40 +26,86 @@ if __name__ == '__main__':
     prev_round = args.round - 1
     curr_round = args.round
     base_dir = os.path.join('/','fedpod','states')
-    if args.algorithm == "fedavg":
-        logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] aggregation algorithm is {args.algorithm.upper()}...")
-        if prev_round < 0:
-            logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] initial model setup from {args.weight_path}...")
-            orig_file = args.weight_path
-            curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
-            center_dir = os.path.join(curr_round_dir, f"{args.job_prefix}{args.inst_id}")
-            os.makedirs(center_dir, exist_ok=True)
-            save_model_path = os.path.join(center_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
-            shutil.copy2(orig_file, save_model_path)
-            logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] initial model setup to {save_model_path}...")
-        else:
-            # base_dir = os.path.join('.','states')
-            prev_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{prev_round:02}")
-            inst_dir = os.path.join(prev_round_dir, f"{args.job_prefix}*") # inst0 also included 
-            pattern = os.path.join(inst_dir, 'models', '*_last.pth') # but, _last.pth removes inst0 because inst0 never has _last.pth file on it
-            pth_path = glob.glob(pattern)
-            for pth in pth_path:
-                logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] local models are from {pth}...")
-            # print(pattern)
-            # print(pth_path)
-            global_state_dict = fedavg(pth_path)
-            state = {'model': global_state_dict}
-            # exp_folder = f"{self.cli_args.exp_name}"
-            # save_model_path1 = os.path.join("states", exp_folder)
-            # save_model_path2 = f"round_{self.round:02d}"
-            # save_model_path3 = f"epoch_{from_epoch:03d}"
-            curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
-            center_dir = os.path.join(curr_round_dir, f"{args.job_prefix}{args.inst_id}")
-            os.makedirs(center_dir, exist_ok=True)
-            save_model_path = os.path.join(center_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
-            torch.save(state, save_model_path)
-            logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] models are aggregated to {save_model_path}...")
+    # base_dir = os.path.join('.','states')
+    
+    logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] aggregation algorithm is {args.algorithm.upper()}...")
+    if prev_round < 0:
+        logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] initial model setup from {args.weight_path}...")
+        orig_file = args.weight_path
+        curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
+        center_dir = os.path.join(curr_round_dir, f"{args.job_prefix}{args.inst_id}")
+        os.makedirs(center_dir, exist_ok=True)
+        save_model_path = os.path.join(center_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
+        shutil.copy2(orig_file, save_model_path)
+        logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] initial model setup to {save_model_path}...")
     else:
-        raise NotImplementedError(f"{args.algorithm.upper()} is not implemented on Aggregator()")
+        prev_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{prev_round:02}")
+        inst_dir = os.path.join(prev_round_dir, f"{args.job_prefix}*") # inst0 also included 
+        pattern = os.path.join(inst_dir, 'models', '*_last.pth') # but, _last.pth removes inst0 because inst0 never has _last.pth file on it
+        pth_path = glob.glob(pattern)
+        for pth in pth_path:
+            logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] local models are from {pth}...")
+        local_models_with_dlen = [torch.load(m) for m in pth_path]
+
+        if args.algorithm == "fedavg":
+            P = [1 for el in local_models_with_dlen]
+            W = [el/sum(P) for el in P]
+            M = [el['model'] for el in local_models_with_dlen]
+            aggregated_model = fedavg(W, M)
+        elif args.algorithm == "fedwavg":
+            P = [1 for el in local_models_with_dlen]
+            W = [el/sum(P) for el in P]
+            M = [el['model'] for el in local_models_with_dlen]
+            aggregated_model = fedwavg(W, M)
+            # averaged_loss = lossavg()
+        else:
+            raise NotImplementedError(f"{args.algorithm.upper()} is not implemented on Aggregator()")
+        state = {'model': aggregated_model}
+        curr_round_dir = os.path.join(base_dir, f"R{args.rounds:02}r{curr_round:02}")
+        center_dir = os.path.join(curr_round_dir, f"{args.job_prefix}{args.inst_id}")
+        os.makedirs(center_dir, exist_ok=True)
+        save_model_path = os.path.join(center_dir, f"R{args.rounds:02}r{curr_round:02}.pth")
+        torch.save(state, save_model_path)
+        logger.info(f"[{args.job_prefix.upper()}][{args.algorithm.upper()}] models are aggregated to {save_model_path}...")
     
 
+
+
+
+            
+        # averaged_loss = lossavg()
+        mean_pre_DSCL_AVG = np.mean([el['pre_metrics']['DSCL_AVG'] for el in local_models_with_dlen])
+        mean_pre_DICE_AVG = np.mean([el['post_metrics']['DICE_AVG'] for el in local_models_with_dlen])
+        mean_pre_HD95_AVG = np.mean([el['post_metrics']['HD95_AVG'] for el in local_models_with_dlen])
+
+        mean_post_DSCL_AVG = np.mean([el['post_metrics']['DSCL_AVG'] for el in local_models_with_dlen])
+        mean_post_DICE_AVG = np.mean([el['post_metrics']['DICE_AVG'] for el in local_models_with_dlen])
+        mean_post_HD95_AVG = np.mean([el['post_metrics']['HD95_AVG'] for el in local_models_with_dlen])
+
+        mean_pre_metrics    = ', '.join(map(str, [mean_pre_DSCL_AVG,   mean_pre_DICE_AVG,  mean_pre_HD95_AVG   ]))
+        mean_post_metrics   = ', '.join(map(str, [mean_post_DSCL_AVG,  mean_post_DICE_AVG, mean_post_HD95_AVG  ]))
+
+
+        logs_dir = os.path.join('/','fedpod','logs')
+        # logs_dir = os.path.join('.','logs')
+        job_dir = os.path.join(logs_dir, f"{args.job_prefix}{args.inst_id}")
+        os.makedirs(job_dir, exist_ok=True)
+        
+        # 컬럼 헤더 정의
+        columns = "mDSCL, mDICE, mHD95"  # 실제 컬럼명에 맞게 수정하세요.
+
+        # Pre-metrics 파일 작성
+        pre_metrics_file = os.path.join(job_dir, f'{args.job_prefix}_pre_metrics.csv')
+        if not os.path.exists(pre_metrics_file):
+            with open(pre_metrics_file, 'a') as f:
+                f.write(columns + '\n')  # 파일이 없으면 헤더 추가
+        with open(pre_metrics_file, 'a') as f:
+            f.write(mean_pre_metrics + '\n')
+
+        # Post-metrics 파일 작성
+        post_metrics_file = os.path.join(job_dir, f'{args.job_prefix}_post_metrics.csv')
+        if not os.path.exists(post_metrics_file):
+            with open(post_metrics_file, 'a') as f:
+                f.write(columns + '\n')  # 파일이 없으면 헤더 추가
+        with open(post_metrics_file, 'a') as f:
+            f.write(mean_post_metrics + '\n')
