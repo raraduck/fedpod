03045a3093c3243a40dbcb0d30852a80d6457676
dwnusa-win10 <dwnusa@gmail.com>
2024-10-11 15:28:04 +0900

    v0.8.11.a test_train.sh

--- test_train.sh
+++ test_train.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-echo "train process has zero-rounds configuration for central mode"
-echo "firstly, non-zero configuration is to be tested"
-./run_train.sh \
-    -s 0 \
-    -g 0 \
-    -j test1 \
-    -R 2 \
-    -r 0 \
-    -E 1 \
-    -i 1
+# echo "::::::::::initial aggregation requires initial_model" && \
+# echo "::::::::::initial_model is just copied to under states/job-step folder" && \
+bash run_train.sh \
+     -s 0 \
+     -g 0 \
+     -j test1 \
+     -R 2 \
+     -r 0 \
+     -E 2 \
+     -i 1 \
+     -m /fedpod/cc359ppmi128/R00E000.pth
+
+# echo "::::::::::second aggregation requires to not specify model_path" && \
+
+# echo "::::::::::which is to collect them(local models) by round and job_prefix pattern" && \
+
+# mkdir -p \
+#     /fedpod/states/R02r00/test1/models \
+#     /fedpod/states/R02r00/test2/models && \
+
+# cp /fedpod/states/R02r00/test0/R02r00.pth /fedpod/states/R02r00/test1/models/R02r00_last.pth && \
+
+# cp /fedpod/states/R02r00/test0/R02r00.pth /fedpod/states/R02r00/test2/models/R02r00_last.pth && \
+
+# bash run_aggregation.sh \
+#     -R 2 \
+#     -r 1 \
+#     -a fedavg \
+#     -j test \
+#     -i 0 \
+#     -m None && \
+    
+# rm -rf /fedpod/states/R02r00/test0 \
+#     /fedpod/states/R02r00/test1 \
+#     /fedpod/states/R02r00/test2 \
+#     /fedpod/states/R02r01/test0 && \
+
+# rmdir /fedpod/states/R02r00 && \
+
+# rmdir /fedpod/states/R02r01
