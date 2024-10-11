37783a873767dbb746e9a35ef6f27c15a54eec43
dwnusa-win10 <dwnusa@gmail.com>
2024-10-11 14:26:18 +0900

    update

--- test_aggregation.sh
+++ test_aggregation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo ">>>>> initial aggregation requires initial_model" && \
-echo ">>>>> initial_model is just copied to under states/job-step folder" && \
+echo "::::::::::initial aggregation requires initial_model" && \
+echo "::::::::::initial_model is just copied to under states/job-step folder" && \
 bash run_aggregation.sh \
     -R 2 \
     -r 0 \
@@ -9,9 +9,9 @@ bash run_aggregation.sh \
     -i 0 \
     -m /fedpod/cc359ppmi128/R00E000.pth && \
 
-echo ">>>>> second aggregation requires to not specify model_path" && \
+echo "::::::::::second aggregation requires to not specify model_path" && \
 
-echo ">>>>> which is to collect them(local models) by round and job_prefix pattern" && \
+echo "::::::::::which is to collect them(local models) by round and job_prefix pattern" && \
 
 mkdir -p \
     /fedpod/states/R02r00/test1/models \
