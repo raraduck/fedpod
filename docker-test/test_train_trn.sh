5af2b1fa9e0623a12905e6c9726bf18f8dcbd73f
dwnusa-win10 <dwnusa@gmail.com>
2024-10-19 23:57:36 +0900

    v0.4.13.14: scheduler and milestones, lr_gamma added on run_train.sh

---
+++ docker-test/test_train_trn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+bash /fedpod/run_train.sh \
+     -s 0 \
+     -g 0 \
+     -J trn_2 \
+     -R 2 \
+     -r 0 \
+     -E 10 \
+     -i 2 \
+     -c /fedpod/cc359ppmi128/CC359PPMI_v1-test.csv \
+     -m /fedpod/cc359ppmi128/R00r00.pth && \
+
+rm -rf /fedpod/states/trn_2/R02r00 && \
+
+rm -rf /fedpod/logs/trn_2_R02r00.log && \
+
+rmdir /fedpod/states/trn_2
