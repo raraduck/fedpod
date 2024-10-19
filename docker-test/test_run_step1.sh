105282d5d6efc1f0b9f8f440a8413c88ee3fc32f
raraduck <raraducker@gmail.com>
2024-10-19 15:41:18 +0900

    update

--- docker-test/test_run_step1.sh
+++ docker-test/test_run_step1.sh
@@ -2,7 +2,7 @@
 echo -e "\033[32mSTART TEST: AGGREGATION\033[0m" && \
 docker-compose -f compose-aggregation.yaml up && \
 echo -e "\033[32mCOMPLETED: AGGREGATION\033[0m" && \
-echo -e "\033[33m--------------------------------\033[0m" && \
+echo -e "\033[33m--------------------------------\033[0m"
 echo -e "\033[32mSTART TEST: INFER\033[0m" && \
 docker-compose -f compose-infer.yaml up && \
 echo -e "\033[32mCOMPLETED: INFER\033[0m"
