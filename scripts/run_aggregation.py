76c89e3bf81ee8da938bab7cca6b0ccf4b1f075a
dwnusa-win10 <dwnusa@gmail.com>
2024-10-23 01:31:19 +0900

    v0.4.14.15: test succeeded on the branch test241023

--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -276,8 +276,6 @@ def main(args):
     # /tmp/next_round.txt 파일에 저장
     with open("/tmp/next_round.txt", "w") as f:
         f.write(next_round_formatted)
-    # with open(f"/fedpod/logs/{args.job_prefix}_R{args.rounds:02}r{args.round:02}_next_round.txt", "w") as f:
-    #     f.write(next_round_formatted)
 
 if __name__ == '__main__': 
     args = parser.parse_args(sys.argv[1:])
