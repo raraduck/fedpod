b74942dac0ed1abb502ef7a4fa0b6eebead14983
dwnm1 <raraducker@gmail.com>
2024-10-23 17:33:13 +0900

    v0.4.14.17: test for run_infer

--- docker-test/test_infer.sh
+++ docker-test/test_infer.sh
@@ -2,16 +2,16 @@
 bash /fedpod/run_infer1.sh \
      -s 1 \
      -g 0 \
-     -J infer_1 \
-     -R 2 \
-     -r 0 \
-     -e 7 \
+     -J cen1 \
+     -R 3 \
+     -r 1 \
+     -e 100 \
      -i 1 \
      -c /fedpod/cc359ppmi128/CC359PPMI_v1-test.csv \
      -m /fedpod/cc359ppmi128/R00r00.pth
 
-rm -rf /fedpod/states/infer_1/R02r00 && \
+rm -rf /fedpod/states/cen1/R02r00 && \
 
-rm -rf /fedpod/logs/infer_1_R02r00.log && \
+rm -rf /fedpod/logs/cen1_R02r00.log && \
 
-rmdir /fedpod/states/infer_1
+rmdir /fedpod/states/cen1
