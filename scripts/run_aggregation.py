583bad2aa0bb13c4312f3118406e977def05f3c7
dwnm1 <raraducker@gmail.com>
2024-10-30 17:14:14 +0900

    v0.4.16.0: aggregation debugging

--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -280,7 +280,7 @@ def fed_processing(args, base_dir, curr_round, next_round, logger, writer):
     if writer is not None:
         for jobname, metrics in jobname_and_metrics:
             for key,value in metrics.items():
-                writer.add_scalar(f"{jobname}/{key}", value, args.round)
+                writer.add_scalar(f"{jobname}/{key}", value['DSCL_AVG'], args.round)
     writer.flush()
     writer.close()
     # TODO: 여기서는 pth last와 prev 를 읽어서 cli_args 내 정보를 바탕으로 pandas 형태로 저장한 뒤 csv에 저장하기
