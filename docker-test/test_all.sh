7aeb43e24775624b51deff1e6c1dd7ad0b44927c
Cloud User <raraducker@gmail.com>
2024-10-30 21:39:26 +0900

    update

--- docker-test/test_all.sh
+++ docker-test/test_all.sh
@@ -3,22 +3,22 @@
 ROUNDS=3 ROUND=0 \
 MODEL=None \
 JOBNAME1=testfed_2 INSTID1=2 \
-docker-compose -f compose-train1.yaml up fedpod-test1
-docker-compose -f compose-train1.yaml down
+docker compose -f compose-train1.yaml up fedpod-test1
+docker compose -f compose-train1.yaml down
 
 ROUNDS=3 ROUND=0 \
 JOBPREFIX=testfed INSTID=0 \
-docker-compose -f compose-aggregation.yaml up
-docker-compose -f compose-aggregation.yaml down
+docker compose -f compose-aggregation.yaml up
+docker compose -f compose-aggregation.yaml down
 
 ROUNDS=3 ROUND=1 \
 MODEL=/fedpod/states/testfed_0/R03r01/models/R03r01_agg.pth \
 JOBNAME1=testfed_2 INSTID1=2 \
 JOBNAME2=testfed_3 INSTID2=3 \
-docker-compose -f compose-train1.yaml up
-docker-compose -f compose-train1.yaml down
+docker compose -f compose-train1.yaml up
+docker compose -f compose-train1.yaml down
 
 ROUNDS=3 ROUND=1 \
 JOBPREFIX=testfed INSTID=0 \
-docker-compose -f compose-aggregation.yaml up
-docker-compose -f compose-aggregation.yaml down
+docker compose -f compose-aggregation.yaml up
+docker compose -f compose-aggregation.yaml down
