f7decb7a8f6e4694b22cadf3c4b9cfc61516c54d
dwnusa-win10 <dwnusa@gmail.com>
2024-10-30 20:06:50 +0900

    v0.4.16.0: test train1 updated for rounds

--- docker-test/test_train1.sh
+++ docker-test/test_train1.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+# round 값을 환경 변수로 전달하여 docker-compose 실행
+ROUNDS=3 ROUND=0 JOBNAME=testfed_2 INSTID=2 \
+docker-compose -f compose-train1.yaml up
+docker-compose -f compose-train1.yaml down
+
+# ROUNDS=3 ROUND=0 JOBNAME=testfed_3 INSTID=3 \
+# docker-compose -f compose-train1.yaml up
+# docker-compose -f compose-train1.yaml down
+
+ROUNDS=3 ROUND=0 JOBPREFIX=testfed INSTID=0 \
+docker-compose -f compose-aggregation.yaml up
+docker-compose -f compose-aggregation.yaml down
+
+
 # 시작 round와 종료 round 설정
-START_ROUND=0
-END_ROUND=3  # 원하는 round 종료값
+# START_ROUND=1
+# END_ROUND=3  # 원하는 round 종료값
 
-for ((round=$START_ROUND; round<$END_ROUND; round++))
-do
-    echo "Starting round $round..."
+# for ((round=$START_ROUND; round<$END_ROUND; round++))
+# do
+#     echo "Starting round $round..."
 
-    # round 값을 환경 변수로 전달하여 docker-compose 실행
-    ROUND=$round docker-compose -f compose-train1.yaml up
+#     # round 값을 환경 변수로 전달하여 docker-compose 실행
+#     ROUND=$round docker-compose -f compose-train1.yaml up
 
-    # 각 round가 완료되면 컨테이너 정리
-    docker-compose -f compose-train1.yaml down
-done
+#     # 각 round가 완료되면 컨테이너 정리
+#     docker-compose -f compose-train1.yaml down
+# done
