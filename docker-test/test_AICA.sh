f493b01dd6863a0a59de89066d2e620f2de8fa2e
dwnusa-win10 <dwnusa@gmail.com>
2024-10-31 04:24:55 +0900

    test for AICA and HPC

--- docker-test/test_AICA.sh
+++ docker-test/test_AICA.sh
@@ -5,25 +5,25 @@ ROUNDS=3 ROUND=0 \
 MODEL=None \
 JOBNAME1=testfed_2 INSTID1=2 \
 JOBNAME2=testfed_3 INSTID2=3 \
-docker compose -f compose-v0.4.16.0.yaml up fedpod-test1 && \
-docker compose -f compose-v0.4.16.0.yaml down
+docker compose -f compose-AICA.yaml up fedpod-test1 && \
+docker compose -f compose-AICA.yaml down
 
 ROUNDS=3 ROUND=0 \
 JOBPREFIX=testfed INSTID=0 \
-docker compose -f compose-v0.4.16.0.yaml up fedpod-aggregation && \
-docker compose -f compose-v0.4.16.0.yaml down
+docker compose -f compose-AICA.yaml up fedpod-aggregation && \
+docker compose -f compose-AICA.yaml down
 
 ROUNDS=3 ROUND=1 \
 MODEL=/fedpod/states/testfed_0/R03r01/models/R03r01_agg.pth \
 JOBNAME1=testfed_2 INSTID1=2 \
 JOBNAME2=testfed_3 INSTID2=3 \
-docker compose -f compose-v0.4.16.0.yaml up fedpod-test1  fedpod-test2 && \
-docker compose -f compose-v0.4.16.0.yaml down
+docker compose -f compose-AICA.yaml up fedpod-test1  fedpod-test2 && \
+docker compose -f compose-AICA.yaml down
 
 ROUNDS=3 ROUND=1 \
 JOBPREFIX=testfed INSTID=0 \
-docker compose -f compose-v0.4.16.0.yaml up fedpod-aggregation && \
-docker compose -f compose-v0.4.16.0.yaml down
+docker compose -f compose-AICA.yaml up fedpod-aggregation && \
+docker compose -f compose-AICA.yaml down
 
 ROUNDS=3 ROUND=1 \
 MODEL=/fedpod/states/testfed_0/R03r02/models/R03r02_agg.pth \
