c30eddbb3e10a9aa0a54610f52d900ac4b02a45a
dwnusa-win10 <dwnusa@gmail.com>
2024-11-05 11:31:52 +0900

    v0.4.16.2->v0.4.16.3: pvd metric as pvdc

--- scripts/utils/metrics.py
+++ scripts/utils/metrics.py
@@ -17,7 +17,7 @@ def cdsc(output: Tensor, target: Tensor) -> np.ndarray:
 def pvdc(output: Tensor, target: Tensor, eps: float = 1e-5) -> np.ndarray:
     """calculate multilabel batch dice"""
     target = target.float()
-    num = 2 * abs(output - target).sum(dim=(2, 3, 4)) + eps
+    num = 2 * (output != target).sum(dim=(2, 3, 4)) + eps
     den = output.sum(dim=(2, 3, 4)) + target.sum(dim=(2, 3, 4)) + eps
     pvd = num / den
 
