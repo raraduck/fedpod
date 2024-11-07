8ee3505af25d6e6757f8bd77034c83e63c098eb5
dwnsnu <dwnsnu@snu.ac.kr>
2024-11-07 13:04:50 +0900

    v0.4.17.1: RobustZScoreNormalization applied only for image not to mask for those of train and val and forward

--- scripts/dsets/dataset_utils.py
+++ scripts/dsets/dataset_utils.py
@@ -145,7 +145,7 @@ def get_forward_transform(args):
         transforms.SpatialPadd(keys=selected_keys, spatial_size=(args.resize, args.resize, args.resize), mode='constant'),
     ]
     base_transform_2 = [
-        RobustZScoreNormalization(keys=args.input_channel_names),
+        RobustZScoreNormalization(keys=args.input_channel_names[0]),
         transforms.ConcatItemsd(keys=args.input_channel_names, name='image', dim=0),
         transforms.DeleteItemsd(keys=args.input_channel_names),
         # ConvertToMultiChannel(keys=["label"], labels=args.label_groups)
