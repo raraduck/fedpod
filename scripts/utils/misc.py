54e00d0553a8e85b6e5ce3a191e1256814e06ccd
dwnusa-win10 <dwnusa@gmail.com>
2024-11-07 21:30:02 +0900

    v0.4.17.2: scaled back to 255 for img and seg

--- scripts/utils/misc.py
+++ scripts/utils/misc.py
@@ -84,12 +84,12 @@ def save_img_nifti(image: Tensor, prefix: str, postfix: str, affine_src: str, mo
             # Convert image tensor to numpy and scale to uint8
             
             image_modality = image_numpy[b][ch_idx].astype(np.float32)
-            if el_modality not in ['seg']:
-                scale = 255
-                # Normalize the image data to 0-255 or 0-100
-                image_modality -= image_modality.min()  # Shift data to 0
-                image_modality /= image_modality.max()  # Normalize to 1
-                image_modality = (image_modality * scale).astype(np.uint8)  # Scale to 0-255 and convert to uint8
+            # if el_modality not in ['seg']:
+            scale = 255
+            # Normalize the image data to 0-255 or 0-100
+            image_modality -= image_modality.min()  # Shift data to 0
+            image_modality /= image_modality.max()  # Normalize to 1
+            image_modality = (image_modality * scale).astype(np.uint8)  # Scale to 0-255 and convert to uint8
 
             nib.save(
                 nib.Nifti1Image(image_modality, affine_src[b]),
