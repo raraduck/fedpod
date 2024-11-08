128d0eb01ccde2a49586da401d6f74b94ab53dda
dwnusa-win10 <dwnusa@gmail.com>
2024-11-09 01:01:06 +0900

    update

---
+++ sequential-jobs.sh
@@ -0,0 +1,9 @@
+argo submit fedpod-test-sol1.yaml -n argo
+while true; do
+  STATUS=$(argo get @latest --no-color | grep Status: | awk '{print $2}')
+  if [[ $STATUS == "Succeeded" ]]; then
+    argo submit fedpod-test-fed.yaml -n argo
+    break
+  fi
+  sleep 10
+done
