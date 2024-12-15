32e8d2c7972d44183ff262f4636e703a4a17c399
dwnusa-win10 <dwnusa@gmail.com>
2024-12-15 20:57:22 +0900

    v0.4.17.15: docker compose

--- docker-test/test_AICA_train1.sh
+++ docker-test/test_AICA_train1.sh
@@ -4,18 +4,18 @@ set -e  # 명령어 실패 시 스크립트 종료
 export ROUNDS=1 ROUND=0 
 export MODEL=None 
 
-export JOBNAME01=centre1 INSTID01=6 
+export JOBNAME01=centre01 INSTID01=6 
 docker compose -f compose-AICA-train1.yaml up centre0-train1 && \
 docker compose -f compose-AICA-train1.yaml down
 
-export JOBNAME02=centre2 INSTID02=6 
+export JOBNAME02=centre02 INSTID02=6 
 docker compose -f compose-AICA-train1.yaml up centre0-train2 && \
 docker compose -f compose-AICA-train1.yaml down
 
-export JOBNAME11=solo1 INSTID11=1
+export JOBNAME11=solo11 INSTID11=1
 docker compose -f compose-AICA-train1.yaml up solo1-train1 && \
 docker compose -f compose-AICA-train1.yaml down
 
-export JOBNAME12=solo1 INSTID12=1
+export JOBNAME12=solo12 INSTID12=1
 docker compose -f compose-AICA-train1.yaml up solo1-train2 && \
 docker compose -f compose-AICA-train1.yaml down
