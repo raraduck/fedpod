29cfc0c7c558a252488ea4972c66dc295920b0fd
dwnusa-win10 <dwnusa@gmail.com>
2024-12-15 21:26:26 +0900

    v0.4.17.15: docker compose

--- docker-test/test_AICA_train.sh
+++ docker-test/test_AICA_train.sh
@@ -4,18 +4,76 @@ set -e  # 명령어 실패 시 스크립트 종료
 export ROUNDS=1 ROUND=0 
 export MODEL=None 
 
-# export JOBNAME01=centre01 INSTID01=6 
-# docker compose -f compose-AICA-train1.yaml up centre0-train1 && \
-# docker compose -f compose-AICA-train1.yaml down
+export JOBNAME01=centre01 INSTID01=6 
+docker compose -f compose-AICA-train.yaml up centre0-train1 && \
+docker compose -f compose-AICA-train.yaml down
 
-# export JOBNAME02=centre02 INSTID02=6 
-# docker compose -f compose-AICA-train1.yaml up centre0-train2 && \
-# docker compose -f compose-AICA-train1.yaml down
+export JOBNAME02=centre02 INSTID02=6 
+docker compose -f compose-AICA-train.yaml up centre0-train2 && \
+docker compose -f compose-AICA-train.yaml down
 
-export JOBNAME11=solo11 INSTID11=1
+
+
+
+export JOBNAME1=solo11 INSTID1=1
 docker compose -f compose-AICA-train.yaml up solo1-train1 && \
 docker compose -f compose-AICA-train.yaml down
 
-export JOBNAME12=solo12 INSTID12=1
+export JOBNAME2=solo12 INSTID2=1
 docker compose -f compose-AICA-train.yaml up solo1-train2 && \
+docker compose -f compose-AICA-train.yaml down
+
+
+
+
+export JOBNAME1=solo21 INSTID1=2
+docker compose -f compose-AICA-train.yaml up solo2-train1 && \
+docker compose -f compose-AICA-train.yaml down
+
+export JOBNAME2=solo22 INSTID2=2
+docker compose -f compose-AICA-train.yaml up solo2-train2 && \
+docker compose -f compose-AICA-train.yaml down
+
+
+
+
+export JOBNAME1=solo31 INSTID1=3
+docker compose -f compose-AICA-train.yaml up solo3-train1 && \
+docker compose -f compose-AICA-train.yaml down
+
+export JOBNAME2=solo32 INSTID2=3
+docker compose -f compose-AICA-train.yaml up solo3-train2 && \
+docker compose -f compose-AICA-train.yaml down
+
+
+
+
+export JOBNAME1=solo41 INSTID1=4
+docker compose -f compose-AICA-train.yaml up solo4-train1 && \
+docker compose -f compose-AICA-train.yaml down
+
+export JOBNAME2=solo42 INSTID2=4
+docker compose -f compose-AICA-train.yaml up solo4-train2 && \
+docker compose -f compose-AICA-train.yaml down
+
+
+
+
+export JOBNAME1=solo51 INSTID1=5
+docker compose -f compose-AICA-train.yaml up solo5-train1 && \
+docker compose -f compose-AICA-train.yaml down
+
+export JOBNAME2=solo52 INSTID2=5
+docker compose -f compose-AICA-train.yaml up solo5-train2 && \
+docker compose -f compose-AICA-train.yaml down
+
+
+
+
+export JOBNAME1=solo61 INSTID1=6
+docker compose -f compose-AICA-train.yaml up solo6-train1 && \
+docker compose -f compose-AICA-train.yaml down
+
+export JOBNAME2=solo62 INSTID2=6
+docker compose -f compose-AICA-train.yaml up solo6-train2 && \
 docker compose -f compose-AICA-train.yaml down
