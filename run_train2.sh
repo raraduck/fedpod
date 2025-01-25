f6d301f9a26b31b4b2a647b850bc5629c17036aa
dwnusa-win10 <dwnusa@gmail.com>
2025-01-25 22:04:08 +0900

    v0.4.18.00: updated run_train1 and forward1 and train2 and forward2

--- run_train2.sh
+++ run_train2.sh
@@ -10,13 +10,15 @@ round=""
 epochs=""
 epoch=""
 inst_id=""
-split_csv="cc359ppmi128/CC359PPMI_v1.csv"
+split_csv=""
 model_pth=""
 data_percentage=""
 data_root=""
+inst_root="inst_*"
+data_set=""
 
 # 명령줄 옵션 처리
-while getopts S:s:f:m:g:J:R:r:E:e:i:c:M:p:D: option
+while getopts S:s:f:m:g:J:R:r:E:e:i:c:M:p:D:d: option
 do
     case "${option}"
     in
@@ -35,14 +37,15 @@ do
         M) model_pth=${OPTARG};;
         p) data_percentage=${OPTARG};;
         D) data_root=${OPTARG};;
+        d) data_set=${OPTARG};;
     esac
 done
-echo "seed: $seed, save_infer: $save_infer, eval_freq: $eval_freq, milestone: $milestone, gpu: $use_gpu, job: $job_name, rounds: $rounds, round: $round, epochs: $epochs, epoch: $epoch, inst: $inst_id, split_csv: $split_csv, model_pth: $model_pth, data_percentage: $data_percentage, data_root: $data_root"
+echo "seed: $seed, save_infer: $save_infer, eval_freq: $eval_freq, milestone: $milestone, gpu: $use_gpu, job: $job_name, rounds: $rounds, round: $round, epochs: $epochs, epoch: $epoch, inst: $inst_id, split_csv: $split_csv, model_pth: $model_pth, data_percentage: $data_percentage, data_root: $data_root, data_root: $data_set"
 
 # 필수 옵션 검사
-if [ -z "$seed" ] || [ -z "$save_infer" ] || [ -z "$eval_freq" ] || [ -z "$milestone" ] ||[ -z "$use_gpu" ] || [ -z "$job_name" ] || [ -z "$rounds" ] || [ -z "$round" ] || [ -z "$epochs" ] || [ -z "$epoch" ] || [ -z "$inst_id" ] || [ -z "$split_csv" ] || [ -z "$model_pth" ] || [ -z "$data_percentage" ] || [ -z "$data_root" ]; then
+if [ -z "$seed" ] || [ -z "$save_infer" ] || [ -z "$eval_freq" ] || [ -z "$milestone" ] ||[ -z "$use_gpu" ] || [ -z "$job_name" ] || [ -z "$rounds" ] || [ -z "$round" ] || [ -z "$epochs" ] || [ -z "$epoch" ] || [ -z "$inst_id" ] || [ -z "$split_csv" ] || [ -z "$model_pth" ] || [ -z "$data_percentage" ] || [ -z "$data_root" ] || [ -z "$data_set" ]; then
     echo "Error: All parameters are required."
-    echo "Usage: $0 -S <seed> -s <save_infer> -f <eval_freq> -m <milestone> -g <use_gpu> -J <job_name> -R <rounds> -r <round> -E <epochs> -e <epoch> -i <inst_id> -c <split_csv> -M <model_pth> -p <data_percentage> -D <data_root>"
+    echo "Usage: $0 -S <seed> -s <save_infer> -f <eval_freq> -m <milestone> -g <use_gpu> -J <job_name> -R <rounds> -r <round> -E <epochs> -e <epoch> -i <inst_id> -c <split_csv> -M <model_pth> -p <data_percentage> -D <data_root> -d <data_set>"
     exit 1
 fi
 
@@ -62,9 +65,9 @@ python3 scripts/run_train.py \
   --patch_size 128 \
   --zoom 1 \
   --flip_lr 0 \
-  --dataset CC359PPMI \
+  --dataset $data_set \
   --data_root $data_root \
-  --inst_root inst_* \
+  --inst_root $inst_root \
   --data_percentage $data_percentage \
   \
   --cases_split $split_csv \
