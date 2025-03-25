2bfb0723a17b082f03851a309b2c72fea0a789fa
dwnusa-win10 <dwnusa@gmail.com>
2025-03-25 22:04:52 +0900

    update: flip_lr arg added on run_train.sh and yaml from00to04 applied for cc359 and fets

--- run_train.sh
+++ run_train.sh
@@ -4,6 +4,7 @@ save_infer=""
 eval_freq=""
 milestone=""
 use_gpu=""
+flir_lr="1"
 job_name=""
 rounds=""
 round=""
@@ -22,7 +23,7 @@ label_names=""
 label_index=""
 
 # 명령줄 옵션 처리
-while getopts S:s:f:m:g:J:R:r:E:e:i:c:M:p:D:d:C:G:N:I: option
+while getopts S:s:f:m:g:L:J:R:r:E:e:i:c:M:p:D:d:C:G:N:I: option
 do
     case "${option}"
     in
@@ -31,6 +32,7 @@ do
         f) eval_freq=${OPTARG};;
         m) milestone=${OPTARG};;
         g) use_gpu=${OPTARG};;
+        L) flir_lr=${OPTARG};;
         J) job_name=${OPTARG};;
         R) rounds=${OPTARG};;
         r) round=${OPTARG};;
@@ -48,12 +50,12 @@ do
         I) label_index=${OPTARG};;
     esac
 done
-echo "seed: $seed, save_infer: $save_infer, eval_freq: $eval_freq, milestone: $milestone, gpu: $use_gpu, job: $job_name, rounds: $rounds, round: $round, epochs: $epochs, epoch: $epoch, inst: $inst_id, split_csv: $split_csv, model_pth: $model_pth, data_percentage: $data_percentage, data_root: $data_root, data_root: $data_set, input_channels: $input_channels, label_groups: $label_groups, label_names: $label_names, label_index: $label_index"
+echo "seed: $seed, save_infer: $save_infer, eval_freq: $eval_freq, milestone: $milestone, gpu: $use_gpu, flip_lr: $flip_lr, job: $job_name, rounds: $rounds, round: $round, epochs: $epochs, epoch: $epoch, inst: $inst_id, split_csv: $split_csv, model_pth: $model_pth, data_percentage: $data_percentage, data_root: $data_root, data_root: $data_set, input_channels: $input_channels, label_groups: $label_groups, label_names: $label_names, label_index: $label_index"
 
 # 필수 옵션 검사
-if [ -z "$seed" ] || [ -z "$save_infer" ] || [ -z "$eval_freq" ] || [ -z "$milestone" ] ||[ -z "$use_gpu" ] || [ -z "$job_name" ] || [ -z "$rounds" ] || [ -z "$round" ] || [ -z "$epochs" ] || [ -z "$epoch" ] || [ -z "$inst_id" ] || [ -z "$split_csv" ] || [ -z "$model_pth" ] || [ -z "$data_percentage" ] || [ -z "$data_root" ] || [ -z "$data_set" ] || [ -z "$input_channels" ] || [ -z "$label_groups" ] || [ -z "$label_names" ] || [ -z "$label_index" ]; then
+if [ -z "$seed" ] || [ -z "$save_infer" ] || [ -z "$eval_freq" ] || [ -z "$milestone" ] ||[ -z "$use_gpu" ] ||[ -z "$flip_lr" ] || [ -z "$job_name" ] || [ -z "$rounds" ] || [ -z "$round" ] || [ -z "$epochs" ] || [ -z "$epoch" ] || [ -z "$inst_id" ] || [ -z "$split_csv" ] || [ -z "$model_pth" ] || [ -z "$data_percentage" ] || [ -z "$data_root" ] || [ -z "$data_set" ] || [ -z "$input_channels" ] || [ -z "$label_groups" ] || [ -z "$label_names" ] || [ -z "$label_index" ]; then
     echo "Error: All parameters are required."
-    echo "Usage: $0 -S <seed> -s <save_infer> -f <eval_freq> -m <milestone> -g <use_gpu> -J <job_name> -R <rounds> -r <round> -E <epochs> -e <epoch> -i <inst_id> -c <split_csv> -M <model_pth> -p <data_percentage> -D <data_root> -d <data_set> -C <input_channels> -G <label_groups> -N <label_names> -I <label_index>"
+    echo "Usage: $0 -S <seed> -s <save_infer> -f <eval_freq> -m <milestone> -g <use_gpu> -L <flip_lr> -J <job_name> -R <rounds> -r <round> -E <epochs> -e <epoch> -i <inst_id> -c <split_csv> -M <model_pth> -p <data_percentage> -D <data_root> -d <data_set> -C <input_channels> -G <label_groups> -N <label_names> -I <label_index>"
     exit 1
 fi
 
@@ -72,7 +74,7 @@ python3 scripts/run_train.py \
   --resize 128 \
   --patch_size 128 \
   --zoom 1 \
-  --flip_lr 1 \
+  --flip_lr $flip_lr \
   --dataset $data_set \
   --data_root $data_root \
   --inst_root $inst_root \
