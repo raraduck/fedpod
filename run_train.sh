a865777ae626aa1655daa4a908119fcc0d1b9f97
raraduck <raraducker@gmail.com>
2026-02-15 23:21:56 +0900

    update

--- run_train.sh
+++ run_train.sh
@@ -75,8 +75,8 @@ python3 scripts/run_train.py \
   --epochs $epochs \
   --epoch $epoch \
   \
-  --resize 168 \
-  --patch_size 168 \
+  --resize 128 \
+  --patch_size 128 \
   --zoom $zoom \
   --flip_lr $flip_lr \
   --dataset $data_set \
